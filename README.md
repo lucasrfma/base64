faf24696c0517c15022d51503eafa9b0cfd28e86
Lucas Romero de Andrade <78609723+lucasrfma@users.noreply.github.com>
2026-06-18 21:56:53 -0300

    Code unification + now decodes via cmd args

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Project done as a way of learning the zig language.
 
 #### Done
  - Encode functionality
-   - Encodes a file: name is received as a single command line argument
  - Decode functionality
+ - Command line arguments:
+   - encode sourcefile encodedfile
+   - decode encodedfile decodedfile 
 
 #### To do
  - Add command line options 
-   - A Decode option, accepts input and output file names.
    - A help option (either '-h' or 'help' as single command line arguments)
--- src/main.zig
+++ src/main.zig
@@ -12,75 +12,25 @@ const ExecutionType = enum {
 };
 
 fn defineExecType(args: []const []const u8) ExecutionType {
-    if (args.len == 2) {
-        if (std.mem.eql(u8, args[1], "-h") or std.mem.eql(u8, args[1], "help")) {
-            return ExecutionType.help;
-        }
-        return ExecutionType.encode;
+    if (args.len == 2 and
+        std.mem.eql(u8, args[1], "-h") or std.mem.eql(u8, args[1], "help")
+    ) {
+        return ExecutionType.help;
     }
 
-    if (args.len >= 3) {
-        return ExecutionType.decode;
+    if (args.len == 4) {
+        if(std.mem.eql(u8, args[1], "-e") or std.mem.eql(u8, args[1], "encode")) {
+
+            return ExecutionType.encode;
+        }
+        if (std.mem.eql(u8, args[1], "-d") or std.mem.eql(u8, args[1], "decode")) {
+
+            return ExecutionType.decode;
+        }
     }
 
     return ExecutionType.invalid;
 }
-
-fn encode(
-    io: std.Io,
-    writer: *std.Io.Writer,
-    fileName: []const u8,
-    allocator: Allocator
-) void {
-    // Get file reader
-    const cwd = std.Io.Dir.cwd();
-    const file = cwd.openFile(io, fileName, .{}) catch {
-        std.log.err("Could not open file =(", .{});
-        return;
-    };
-    defer file.close(io);
-    var file_reader = file.reader(io, &.{});
-    const reader = &file_reader.interface;
-
-    // multiple of 3, since we encode in triplets.
-    var read_buffer: [2100]u8 = undefined;
-
-    while (true) {
-        const len = reader.readSliceShort(&read_buffer) catch {
-            std.log.err("Could not read from file",.{});
-            return;
-        };
-        const output = base64.encode(read_buffer[0..len], allocator) catch {
-            std.log.err("Could not encode.",.{});
-            return;
-        };
-        writer.print("{s}",.{output}) catch {
-            std.log.err("Could not output the result.",.{});
-            return;
-        };
-        if (len < read_buffer.len or len == 0) break;
-    }
-}
-
-// fn decode(
-//     io: std.Io,
-//     writer: *std.Io.Writer,
-//     inputFileName: []const u8,
-//     outputFileName: []const u8,
-//     allocator: Allocator
-// ) void {
-//     // Get file reader
-//     const cwd = std.Io.Dir.cwd();
-//     const file = cwd.openFile(io, inputFileName, .{}) catch {
-//         std.log.err("Could not open file =(", .{});
-//         return;
-//     };
-//     defer file.close(io);
-//     var file_reader = file.reader(io, &.{});
-//     const reader = &file_reader.interface;
-
-
-// }
 
 pub fn main(init: std.process.Init) !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -89,11 +39,7 @@ pub fn main(init: std.process.Init) !void {
     const allocator = arena.allocator();
 
     const io = init.io;
-    // Get stdout Writer
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-    defer stdout_writer.flush() catch {}; // Don't forget to flush!
+    const start = std.Io.Clock.awake.now(io);
 
     // get console args
     const args = init.minimal.args.toSlice(allocator) catch {
@@ -105,11 +51,80 @@ pub fn main(init: std.process.Init) !void {
     }
     const execType = defineExecType(args);
 
-    const start = std.Io.Clock.awake.now(io);
     switch (execType) {
-        .encode => encode(io, stdout_writer, args[1], allocator),
-        // .decode => decode(io, stdout_writer, args[1], args[2], allocator),
-        else => return,
+        .encode,
+        .decode => {},
+        .help => {
+            std.log.info("Print help.",.{});
+            return;
+        },
+        .invalid => {
+            std.log.info("Bad arguments, also print help.", .{});
+            return;
+        },
+    }
+    
+    const cwd = std.Io.Dir.cwd();
+    const outputFile = cwd.createFile(io, args[3], .{}) catch {
+        std.log.err("Could not create output file", .{});
+        return;
+    };
+    defer outputFile.close(io);
+    var file_writer = outputFile.writer(io, &.{});
+    const writer = &file_writer.interface;
+    
+    // Get file reader
+    const file = cwd.openFile(io, args[2], .{}) catch |err| switch (err) {
+        error.FileNotFound => {
+            std.log.err("Could not find input file {s}", .{args[1]});
+            return;
+        },
+        error.AccessDenied => {
+            std.log.err("Access denied to input file {s}", .{args[1]});
+            return;
+        },
+        else => {
+            std.log.err("Could not open input file {s}", .{args[1]});
+            return;
+        }
+    };
+    defer file.close(io);
+    var file_reader = file.reader(io, &.{});
+    const reader = &file_reader.interface;
+
+    var read_buffer: []u8 = undefined;
+
+    var function: *const fn([]const u8, Allocator) Allocator.Error![]u8 = undefined;
+    var message: []const u8 = undefined;
+    switch (execType) {
+        .encode => {
+            function = base64.encode;
+            message = "Encode";
+            read_buffer = try allocator.alloc(u8, 4095);
+        },
+        .decode => {
+            function = base64.decode;
+            message = "Decode";
+            read_buffer = try allocator.alloc(u8, 4096);
+        },
+        else => unreachable
+    }
+
+    while (true) {
+        const len = reader.readSliceShort(read_buffer) catch {
+            std.log.err("Could not read from file",.{});
+            return;
+        };
+        const output = function(read_buffer[0..len], allocator) catch {
+            std.log.err("Could not {s}.",.{message});
+            return;
+        };
+        std.log.info("{s}d:\n{s}",.{message, output});
+        _ = writer.write(output) catch {
+            std.log.err("Could not output the result.",.{});
+            return;
+        };
+        if (len < read_buffer.len or len == 0) break;
     }
 
     const elapsed = start.untilNow(io, .awake);
